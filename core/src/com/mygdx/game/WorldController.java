06e6a89657adaa7d1b9583a40751b5782a4204f0
Aaron Gerber <aasherknight@gmail.com>
2018-10-31 10:25:47 -0400

    adding World Controller

---
+++ core/src/com/mygdx/game/WorldController.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+/**
+ * Controls how the world updates
+ * @author AaronGerber
+ *
+ */
+
+public class WorldController
+{
+	private static final String TAG = WorldController.class.getName();
+	
+	public WorldController()
+	{
+		//TODO
+	}
+	
+	private void init()
+	{
+		//TODO
+	}
+	
+	/**
+	 * Updates based on how much time has passed since the last update.
+	 * @param deltaTime How much time has passed
+	 */
+	public void update(float deltaTime)
+	{
+		//TODO
+	}
+}
