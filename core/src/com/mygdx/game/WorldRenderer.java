cb4b56a84dcafab1b1d4c10b48245b7daac5aa49
Aaron Gerber <aasherknight@gmail.com>
2018-10-31 10:29:26 -0400

    adding worldrenderer

---
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -0,0 +1,37 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Displays the game world to the player
+ * @author Aaron Gerber
+ *
+ */
+
+public class WorldRenderer
+{
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private WorldController worldController;
+	
+	public WorldRenderer(WorldController worldController)
+	{
+		//TODO
+	}
+	
+	private void init()
+	{
+		//TODO
+	}
+	
+	public void render()
+	{
+		//TODO
+	}
+	
+	public void resize(int width, int height)
+	{
+		//TODO
+	}
+}
