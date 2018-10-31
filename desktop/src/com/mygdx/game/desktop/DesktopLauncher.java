81c82d03328f62ed1a980f6ab1ffdb1f82ecf352
Aaron Gerber <aasherknight@gmail.com>
2018-10-31 11:44:09 -0400

    adding the texture atlas

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,33 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.mygdx.game.CSC361_F18_Gerber;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher 
+{
+	private static boolean rebuildAtlas = false;
+	private static boolean drawDebugOutline = true;
+	
+	public static void main (String[] arg) 
+	{
+		if(rebuildAtlas)
+		{
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "../core/assets/art","../core/assets","slimy.atlas");
+		}
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Slimy";
+		config.width = 800;
+		config.height = 480;
+		
 		new LwjglApplication(new CSC361_F18_Gerber(), config);
+		
 	}
 }
