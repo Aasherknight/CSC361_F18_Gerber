7177db0892ba4f09d883b83d0f537ad44e5449fe
Aaron Gerber <aasherknight@gmail.com>
2018-11-07 12:21:51 -0500

    add level to the world controller

--- core/src/com/mygdx/game/WorldController.java
+++ core/src/com/mygdx/game/WorldController.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.util.CameraHelper;
+import com.mygdx.game.util.Constants;
 
 /**
  * Controls how the world updates
@@ -20,10 +21,14 @@ public class WorldController extends InputAdapter
 {
 	private static final String TAG = WorldController.class.getName();
 	
-	public Sprite[] testSprites;
-	public int selectedSprite;
+//	public Sprite[] testSprites;
+//	public int selectedSprite;
 	
 	public CameraHelper cameraHelper;
+	
+	public Level level;
+	public int lives;
+	public int score;
 	
 	public WorldController()
 	{
@@ -34,7 +39,15 @@ public class WorldController extends InputAdapter
 	{
 		Gdx.input.setInputProcessor(this);
 		cameraHelper = new CameraHelper();
-		initTestObjects();
+		//initTestObjects();
+		lives = Constants.LIVES_START;
+		initLevel();
+	}
+	
+	public void initLevel()
+	{
+		score = 0;
+		level = new Level(Constants.LEVEL_01);
 	}
 	
 	/**
@@ -44,64 +57,64 @@ public class WorldController extends InputAdapter
 	public void update(float deltaTime)
 	{
 		handleDebugInput(deltaTime);
-		updateTestObjects(deltaTime);
+		//updateTestObjects(deltaTime);
 		cameraHelper.update(deltaTime);
 	}
 	
-	private void initTestObjects()
-	{
-		//array of 5 sprites
-		testSprites = new Sprite[5];
-		//list of texture regions
-		Array<TextureRegion> regions = new Array<TextureRegion>();
-		regions.add(Assets.instance.slimy.slimy);
-		regions.add(Assets.instance.imp.imp);
-		regions.add(Assets.instance.jelly.jelly);
-		regions.add(Assets.instance.dividing_jelly.dividing_jelly);
-		
-		//create new sprites using the random texture region
-		for(int i = 0; i < testSprites.length; i++)
-		{
-			Sprite spr = new Sprite(regions.random());
-			spr.setSize(1, 1);
-			//set origin for sprites center
-			spr.setOrigin(spr.getWidth()/2.0f, spr.getHeight()/2.0f);
-			//calculate random position for sprite
-			float ranX = MathUtils.random(-2.0f, 2.0f);
-			float ranY = MathUtils.random(-2.0f, 2.0f);
-			spr.setPosition(ranX, ranY);
-			
-			//put the sprite we made into array
-			testSprites[i] = spr;
-		}
-		//select the first sprite
-		selectedSprite = 0;
-	}
-	
-	private void updateTestObjects(float deltaTime)
-	{
-		float rotation = testSprites[selectedSprite].getRotation();
-		
-		rotation += 90*deltaTime;
-		rotation %= 360;
-		
-		testSprites[selectedSprite].setRotation(rotation);
-	}
+//	private void initTestObjects()
+//	{
+//		//array of 5 sprites
+//		testSprites = new Sprite[5];
+//		//list of texture regions
+//		Array<TextureRegion> regions = new Array<TextureRegion>();
+//		regions.add(Assets.instance.slimy.slimy);
+//		regions.add(Assets.instance.imp.imp);
+//		regions.add(Assets.instance.jelly.jelly);
+//		regions.add(Assets.instance.dividing_jelly.dividing_jelly);
+//		
+//		//create new sprites using the random texture region
+//		for(int i = 0; i < testSprites.length; i++)
+//		{
+//			Sprite spr = new Sprite(regions.random());
+//			spr.setSize(1, 1);
+//			//set origin for sprites center
+//			spr.setOrigin(spr.getWidth()/2.0f, spr.getHeight()/2.0f);
+//			//calculate random position for sprite
+//			float ranX = MathUtils.random(-2.0f, 2.0f);
+//			float ranY = MathUtils.random(-2.0f, 2.0f);
+//			spr.setPosition(ranX, ranY);
+//			
+//			//put the sprite we made into array
+//			testSprites[i] = spr;
+//		}
+//		//select the first sprite
+//		selectedSprite = 0;
+//	}
+//	
+//	private void updateTestObjects(float deltaTime)
+//	{
+//		float rotation = testSprites[selectedSprite].getRotation();
+//		
+//		rotation += 90*deltaTime;
+//		rotation %= 360;
+//		
+//		testSprites[selectedSprite].setRotation(rotation);
+//	}
 
 	private void handleDebugInput(float deltaTime)
 	{
 		if(Gdx.app.getType() != ApplicationType.Desktop) return;
 		
-		float sprMoveSpeed = 5*deltaTime;
-		
-		if (Gdx.input.isKeyPressed(Keys.A))
-			moveSelectedSprite(-sprMoveSpeed, 0);
-		if (Gdx.input.isKeyPressed(Keys.D))
-			moveSelectedSprite(sprMoveSpeed, 0);
-		if (Gdx.input.isKeyPressed(Keys.W))
-			moveSelectedSprite(0, sprMoveSpeed);
-		if (Gdx.input.isKeyPressed(Keys.S))
-			moveSelectedSprite(0, -sprMoveSpeed);
+//		float sprMoveSpeed = 5*deltaTime;
+//		
+//		if (Gdx.input.isKeyPressed(Keys.A))
+//			moveSelectedSprite(-sprMoveSpeed, 0);
+//		if (Gdx.input.isKeyPressed(Keys.D))
+//			moveSelectedSprite(sprMoveSpeed, 0);
+//		if (Gdx.input.isKeyPressed(Keys.W))
+//			moveSelectedSprite(0, sprMoveSpeed);
+//		if (Gdx.input.isKeyPressed(Keys.S))
+//			moveSelectedSprite(0, -sprMoveSpeed);
 		
 		// Camera Controls (move)
 		float camMoveSpeed = 5 * deltaTime;
@@ -139,10 +152,10 @@ public class WorldController extends InputAdapter
 		cameraHelper.setPosition(x,y);
 	}
 	
-	private void moveSelectedSprite(float x, float y)
-	{
-		testSprites[selectedSprite].translate(x, y);
-	}
+//	private void moveSelectedSprite(float x, float y)
+//	{
+//		testSprites[selectedSprite].translate(x, y);
+//	}
 	
 	@Override
 	public boolean keyUp(int keycode)
@@ -154,20 +167,20 @@ public class WorldController extends InputAdapter
 			Gdx.app.debug(TAG, "Game world reset");
 		}
 		
-		//select next sprite
-		else if(keycode == Keys.SPACE)
-		{
-			selectedSprite = (selectedSprite+1)%testSprites.length;
-			if(cameraHelper.hasTarget())
-				cameraHelper.setTarget(testSprites[selectedSprite]);
-			Gdx.app.debug(TAG, "Sprite #" + selectedSprite + " selected");
-		}
+//		//select next sprite
+//		else if(keycode == Keys.SPACE)
+//		{
+//			selectedSprite = (selectedSprite+1)%testSprites.length;
+//			if(cameraHelper.hasTarget())
+//				cameraHelper.setTarget(testSprites[selectedSprite]);
+//			Gdx.app.debug(TAG, "Sprite #" + selectedSprite + " selected");
+//		}
 		
-		else if (keycode == Keys.ENTER)
-		{
-			cameraHelper.setTarget((cameraHelper.hasTarget() ? null : testSprites[selectedSprite]));
-			Gdx.app.debug(TAG, "Camera follow enabled: " + cameraHelper.hasTarget());
-		}
+//		else if (keycode == Keys.ENTER)
+//		{
+//			cameraHelper.setTarget((cameraHelper.hasTarget() ? null : testSprites[selectedSprite]));
+//			Gdx.app.debug(TAG, "Camera follow enabled: " + cameraHelper.hasTarget());
+//		}
 		return false;
 	}
 }
--- core/src/com/mygdx/game/util/Constants.java
+++ core/src/com/mygdx/game/util/Constants.java
@@ -12,5 +12,17 @@ public class Constants
 	// Visible game world is 5 meters tall
 	public static final float VIEWPORT_HEIGHT = 5.0f;
 	
+	//GUI width
+	public static final float VIEWPORT_GUI_WIDTH = 800.0f;
+	
+	//GUI height
+	public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
+	
 	public static final String TEXTURE_ATLAS_OBJECTS = "../core/assets/slimy.atlas";
+	
+	//location for level 1's image
+	public static final String LEVEL_01 = "../core/assets/level01.png";
+	
+	//slimes only have one life
+	public static final int LIVES_START = 1;
 }
